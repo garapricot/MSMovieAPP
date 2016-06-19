8928c505d178932c8284b33d2b6fe673c4f298f7
Garegin Hambardzumian <yerevanrockcity@gmail.com>
2016-06-19 11:30:02 +0400

    Adding View, set data ina ViewBag object for dinamicly passing from controller to View

--- MVCMSMovieapp/Controllers/HelloWorldController.cs
+++ MVCMSMovieapp/Controllers/HelloWorldController.cs
@@ -5,20 +5,16 @@ namespace MVCMSMovieapp.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/ 
-
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name,int numtimes=1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numtimes;
+            return View();
         }
     }
 }
