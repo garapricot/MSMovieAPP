afb74b79d3148a1b6a4ea2e4a89f797cf447f04d
Garegin Hambardzumian <yerevanrockcity@gmail.com>
2016-06-19 16:09:28 +0400

    Examining the Edit Methods and Edit View

--- MVCMSMovieapp/Models/Movie.cs
+++ MVCMSMovieapp/Models/Movie.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace MVCMSMovieapp.Models
@@ -7,6 +9,9 @@ namespace MVCMSMovieapp.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [DisplayName("Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public int Price { get; set; }
